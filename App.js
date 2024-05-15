72d0b666a97e0639d1c58bbe2cdea352e3d0d9fd
kamildegerliyurt <degerliyurtkamil@gmail.com>
2024-05-15 16:26:17 +0300

    button add

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+import { LinearGradient } from 'expo-linear-gradient';
+
+import AwesomeButton from "react-native-really-awesome-button";
+import { ThemedButton } from 'react-native-really-awesome-button';
+import { FontAwesome } from '@expo/vector-icons';
+
+
+const App = () => {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* <AwesomeButton>Press</AwesomeButton>
+      <AwesomeButton type="primary">Press</AwesomeButton>
+      <AwesomeButton type="secondary">Secondary</AwesomeButton>
+      <AwesomeButton type="anchor">Anchor</AwesomeButton> */}
+      {/* <AwesomeButton type="danger">Danger</AwesomeButton> */}
+
+
+      <AwesomeButton 
+          extra={
+                 <LinearGradient  colors={["#4C63D2", 
+                                           "#BC3081", 
+                                           "#F47133", 
+                                           "#FED576"]}
+                                  style={{ ...StyleSheet.absoluteFillObject }}
+                 />
+                }>
+
+                  <Text>Press1</Text> 
+      </AwesomeButton>
+
+
+
+
+
+      <ThemedButton name="rick" type="primary">Press2</ThemedButton>
+
+
+
+      <ThemedButton name="bruce" type="primary">Press3</ThemedButton>
+
+
+
+      <ThemedButton name="bruce"
+                    type="primary"
+                    before={<FontAwesome name="trash-o" 
+                                         size={24} 
+                                         color="red" />}> Press4
+      </ThemedButton>
+
+
+
+      <ThemedButton name="bruce"
+                    type="primary"
+                    after={<FontAwesome name="trash-o" 
+                                         size={24} 
+                                         color="red" />}> Press5
+      </ThemedButton>
+
+
+
+
+
     </View>
-  );
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"#fff",
   },
-});
+  buttonContainer: {
+    borderWidth:2,
+    width:"20%",
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:10,
+    padding:5,
+    borderRadius:10,
+  }
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+IMPORT
+
+npx expo install expo-linear-gradient
+npm install --save react-native-really-awesome-button
